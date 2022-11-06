789ab2a6f731be8866f653a1a8e53a45842c550c
tammytdo11 <tammytdo11@gmail.com>
2022-11-06 09:46:55 -0800

    Added brower router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Auth0Provider } from "@auth0/auth0-react";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  
+    <BrowserRouter>
+    <Auth0Provider
+      domain={process.env.REACT_APP_AUTH_DOMAIN}
+      clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
+      redirectUri={process.env.REACT_APP_AUTH_REDIRECT_URI}
+    >
+      <App />
+    </Auth0Provider>
+    </BrowserRouter>
+  // </React.StrictMode>
 );
