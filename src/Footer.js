f1374db8a9270c7062a904979afcf8c4d0bffcd7
tammytdo11 <tammytdo11@gmail.com>
2022-11-19 19:41:05 -0800

    completed login and overall cleanup

--- src/Footer.js
+++ src/Footer.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
+import "./App.css";
 
 class Footer extends React.Component {
   render() {
     return (
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand>Code Fellows</Navbar.Brand>
+      <Navbar className="footer" collapseonselect="true" expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand>Bookshelf 2022</Navbar.Brand>
       </Navbar>
     )
   }
